3afc2c219b74164026c990a4197da59989d40542
Sharad Mishra <sharadrx9@gmail.com>
2025-04-17 19:30:39 +0000

    Created signup logic and implemented envryption for passwords

---
+++ src/repository/user-repository.js
@@ -0,0 +1,28 @@
+const { User } = require('../models/index');
+
+class UserRepository {
+    async create(data){
+        try{
+            const user = await User.create(data);
+            return user;
+        }catch(error){
+            console.error("Something went wrong in the repository layer ...");
+            throw error;
+        }
+    }
+    async destroy(userId){
+        try{
+            const user = await User.destroy({
+                where: {
+                    id: userId
+                }
+            });
+            return user;
+        }catch(error){
+            console.log("Something went wrong in the repository layer ...");
+            throw error;
+        }
+    }
+}
+
+module.exports = UserRepository ;

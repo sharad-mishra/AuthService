5cb428dcc55169bc4cfa0f93768432495f9417ef
Sharad Mishra <sharadrx9@gmail.com>
2025-04-17 12:51:35 +0000

    Updated src/index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 const prepareAndStartServer = () => {
 
     app.listen(PORT, () => {
-        console.log(`Server is running on PORT 3001.`);
+        console.log(`Server is running on PORT ${PORT}.`)
     })
 }
 

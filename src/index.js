618904eda98ce13efef1307fe6e65b62a4902e15
Sharad Mishra <sharadrx9@gmail.com>
2025-04-18 18:22:17 +0000

    Added getUserById in repository code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
+//const UserRepository = require('./repository/user-repository');
 
 const app = express();
 
@@ -11,8 +12,11 @@ const prepareAndStartServer = () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use('/api',apiRoutes);
-    app.listen(PORT, () => {
-        console.log(`Server is running on PORT ${PORT}.`)
+    app.listen(PORT, async() => {
+        console.log(`Server is running on PORT ${PORT}.`);
+        // const repo = new UserRepository();
+        // const response = await repo.getById(2);
+        // console.log(response);
     });
 }
 
